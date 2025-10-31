6944d2250d6a4e753ffd24b189eef5c06f1064d0
Humza920 <humza.rehman.779@gmail.com>
2025-10-31 15:44:10 +0500

    CRETING

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: "https://expense-tracker-frontend-dj1s.vercel.app",
+    origin: "https://expense-tracker-frontend-seven-lyart.vercel.api",
     credentials:true
 }))
 

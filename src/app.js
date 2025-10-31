41a6ae6d436785e4ded46db4e51c96736d0e0402
Humza920 <humza.rehman.779@gmail.com>
2025-10-31 17:08:51 +0500

    CRETING

--- src/app.js
+++ src/app.js
@@ -10,10 +10,15 @@ const dashboardRouter = require("./routes/dashboardRoutes")
 const app = express()
 app.use(express.json())
 
-app.use(cors({
-    origin: "https://expense-tracker-frontend-seven-lyart.vercel.app",
+app.use(
+  cors({
+    origin: [
+      "https://expense-tracker-frontend-seven-lyart.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
-}))
+  })
+);
 
 
 app.use(cookieParser())
